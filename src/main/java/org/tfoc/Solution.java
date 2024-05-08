851a919f5593ce1f54b37760ee394e0b1e4bd8ea
Victor Buralla <vburalla@mercadona.es>
2024-05-08 16:34:24 +0200

    feat: first solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,5 +1,7 @@
 package org.tfoc;
 
+import static java.lang.Math.min;
+
 /**
  * The class containing the solution to this exercise
  */
@@ -7,7 +9,29 @@ public class Solution {
 
     public int maxArea(int[] height) {
 
-      return 0;
+        int maxVolume = 0;
+        if(height.length > 1) {
+            int volume = 0;
+            int leftMax = 0;
+            int rightMin = height.length;
+            int i=0;
+            while(i < rightMin) {
+                if(height[i] > leftMax) {
+                    leftMax = height[i];
+                    int j = height.length;
+                    do {
+                        j--;
+                        if(j < rightMin)
+                            rightMin = j;
+                        volume = min(height[j], leftMax) * (j - i);
+                        if (volume > maxVolume)
+                            maxVolume = volume;
+                    } while (height[j] < leftMax && j > i);
+                }
+                i++;
+            }
+        }
+        return maxVolume;
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,40 @@
 package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 class SolutionTest {
 
-    @Test
-    void testSolution() {
+    static Solution solution;
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+    @BeforeAll
+    static void setUp() {
+        solution = new Solution();
     }
 
+    @ParameterizedTest
+    @MethodSource("provideLinkedLists")
+    void testSolution(int[] height, int volume) {
+
+        assertEquals(volume, solution.maxArea(height));
+    }
+
+    private static Stream<Arguments> provideLinkedLists() {
+
+        return Stream.of(
+                Arguments.of(new int[]{1,8,6,2,5,4,8,3,7}, 49),
+                Arguments.of(new int[]{1,1}, 1),
+                Arguments.of(new int[]{}, 0),
+                Arguments.of(new int[]{10}, 0)
+        );
+    }
 }
